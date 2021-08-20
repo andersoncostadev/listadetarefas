3dbc0dcd3263be4a243c730c93e04857ec9a0507
andersoncostadev <78692782+andersoncostadev@users.noreply.github.com>
2021-08-20 06:15:20 -0300

    primeiro commit

---
+++ src/data/mock.ts
@@ -0,0 +1,11 @@
+import Tarefa from '../model/Tarefa'
+import ListaTarefas from '../model/ListaTarefas'
+import TipoFiltro from '../model/TipoFiltro'
+
+const tarefasIniciais: Tarefa[] = [
+  Tarefa.criarAtiva(1, 'Estudar Next'),
+  Tarefa.criarConcluida(2, 'Limpar Carro'),
+  Tarefa.criarAtiva(3, 'Comprar livro do mês')
+]
+
+export default new ListaTarefas(tarefasIniciais, TipoFiltro.NENHUM)

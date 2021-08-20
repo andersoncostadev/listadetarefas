1996b1b443e1fec94b789175f0e60d6fc851616c
andersoncostadev <78692782+andersoncostadev@users.noreply.github.com>
2021-08-20 06:34:24 -0300

    Correção da integração com Tailwind css

--- src/data/mock.ts
+++ src/data/mock.ts
@@ -3,9 +3,9 @@ import ListaTarefas from '../model/ListaTarefas'
 import TipoFiltro from '../model/TipoFiltro'
 
 const tarefasIniciais: Tarefa[] = [
-  Tarefa.criarAtiva(1, 'Estudar Next'),
-  Tarefa.criarConcluida(2, 'Limpar Carro'),
-  Tarefa.criarAtiva(3, 'Comprar livro do mês')
+  //Tarefa.criarAtiva(1, 'Estudar Next'),
+  // Tarefa.criarConcluida(2, 'Limpar Carro'),
+  // Tarefa.criarAtiva(3, 'Comprar livro do mês')
 ]
 
 export default new ListaTarefas(tarefasIniciais, TipoFiltro.NENHUM)
